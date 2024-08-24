a317e341df0e778f7b77cd1087f446017b97ca99
Kevin Quispe Rojas <kquispe07@hotmail.com>
2024-08-24 17:45:25 -0500

    Recursos de Microservicios
    -Script de base de datos

---
+++ Script Database/Query Insert Pelicula.sql
@@ -0,0 +1,36 @@
+insert into pelicula(price, rating, description, idiomas, image, title, youtube_url)
+	values(20, 4, "Edgar-Jones interpreta a Kate Cooper, una antigua cazadora de tormentas atormentada por un encuentro devastador con un tornado durante sus años universitarios, que 		ahora estudia patrones de tormentas en pantallas de forma segura en la ciudad de Nueva York. Es atraída de nuevo a las llanuras abiertas por su amigo, Javi (Anthony Ramos), 		para probar un nuevo sistema revolucionario de seguimiento de tornados. Allí, se cruza en el camino con Tyler Owens (Powell), el encantador e imprudente superestrella de 		las redes sociales que prospera al publicar sus aventuras cazando tormentas con su gran equipo, cuanto más peligroso, mejor. A medida que la temporada de tormentas se 			intensifica, se desatan fenómenos aterradores nunca antes vistos, y Kate, Tyler y sus equipos competidores se encuentran directamente en los caminos de múltiples sistemas 		de tormentas convergiendo sobre el centro de Oklahoma en la batalla de sus vidas."
+		, "Español", "/images/peli3.jpg","Tornados", "https://www.youtube.com/watch?v=kFQPaJnUyq8");
+
+insert into pelicula(price, rating, description, idiomas, image, title, youtube_url)
+	values(30, 2, "Varios niños de una pequeña ciudad del estado de Maine se alían para combatir a una entidad diabólica que adopta la forma de un payaso y desde hace mucho tiempo 		emerge cada 27 años para saciarse de sangre infantil.",
+		"Español", "/images/peli5.jpg", "IT", "https://www.youtube.com/watch?v=FnCdOQsX5kc");
+
+insert into pelicula(price, rating, description, idiomas, image, title, youtube_url)
+	values(20,3,"Una monja estadounidense llega a un convento de la campiña italiana. Allí descubre que, a pesar de ser virgen, está embarazada. Al mismo tiempo, comienza a sospechar 		que algo perverso ocurre entre los muros del convento.",
+    		"Español, Ingles", "/images/peli6.jpg","Inmaculada", "https://www.youtube.com/watch?v=Q5nHrFXUUc8");
+    
+insert into pelicula(price, rating, description, idiomas, image, title, youtube_url)
+	values(90,4.3,"El aspirante a campeón de carreras Rayo McQueen parece que está a punto de conseguir el éxito. Su actitud arrogante se desvanece cuando llega a una pequeña comunidad 		olvidada que le enseña las cosas importantes de la vida que había olvidado.",
+		"Español", "/images/peli4.jpg", "Cars", "https://www.youtube.com/watch?v=W_H7_tDHFE8");
+
+insert into pelicula(price, rating, description, idiomas, image, title, youtube_url)
+	values(20,4.4,"En su segundo año luchando contra el crimen, Batman explora la corrupción existente en la ciudad de Gotham y el vínculo de esta con su propia familia. Además, 			entrará en conflicto con un asesino en serie conocido como el Acertijo.",
+    		"Español", "/images/peli7.jpg", "Batman", "https://www.youtube.com/watch?v=fWQrd6cwJ0A");
+    
+insert into pelicula(price, rating, description, idiomas, image, title, youtube_url)
+	values(20,5,"Marvel Studios presenta su mayor equivocación hasta la fecha: DEADPOOL & WOLVERINE. Un apático Wade Wilson se esfuerza por adaptarse a la vida civil. Sus días como el 		mercenario moralmente flexible, Deadpool, han quedado atrás. Cuando su mundo se enfrenta a una amenaza existencial, reaciamente Wade debe ponerse el traje de nuevo, junto 		con un aún más reacio… ¿muy reacio? ¿reacísimo?... Tiene que convencer a un súper-reacio Wolverine a... Mierda. Las sinopsis son tan increíblemente estúpidas.",
+    		"Español, Ingles","/images/peli1.jpg", "Deadpool y Wolverine", "https://www.youtube.com/watch?v=UzFZR2dRsSY");
+    
+insert into pelicula(price, rating, description, idiomas, image, title, youtube_url)
+	values(400, 5.9, "Hace mucho tiempo, en una lejana ciénaga, vivía un ogro llamado Shrek. Un día, su preciada soledad se ve interrumpida por un montón de personajes de cuento de 		hadas que invaden su casa. Todos fueron desterrados de su reino por el malvado Lord Farquaad. Decidido a devolverles su reino y recuperar la soledad de su ciénaga, Shrek 		llega a un acuerdo con Lord Farquaad y va a rescatar a la princesa Fiona, la futura esposa del rey. Sin embargo, la princesa esconde un oscuro secreto.",
+    		"Español, Ingles", "/images/peli8.jpg", "Shrek", "https://www.youtube.com/watch?v=CwXOrWvPBPk");
+    
+insert into pelicula(price, rating, description, idiomas, image, title, youtube_url)
+	values(300, 5, "Un grupo de científicos y exploradores, encabezados por Cooper, se embarcan en un viaje espacial para encontrar un lugar con las condiciones necesarias para 			reemplazar a la Tierra y comenzar una nueva vida allí. La Tierra está llegando a su fin y este grupo necesita encontrar un planeta más allá de nuestra galaxia que garantice 		el futuro de la raza humana.",
+    		"Español, Ingles", "/images/peli9.jpg", "Interestellar", "https://www.youtube.com/watch?v=UoSSbmD9vqc");
+
+insert into pelicula(price, rating, description, idiomas, image, title, youtube_url)
+	values(20,3.5, "Pierre Coffin regresa como la emblemática voz de los Minions y el nominado al Oscar® Steve Coogan pone de nuevo voz en VO a Silas McCulloch. Cargada de acción 			trepidante y rebosante del característico humor subversivo de Illumination, GRU 4 MI VILLANO FAVORITO está dirigida por el cocreador de los Minions, el nominado al Oscar® 		Chris Renaud (Gru 2, mi villano favorito, Mascotas), y producida por el visionario fundador y director ejecutivo de Illumination, Chris Meledandri, así como por Brett 			Hoffman (productor ejecutivo de Super Mario Bros: La película y Minions: El origen de Gru). Codirige la cinta Patrick Delage (director de animación de ¡Canta! 2 y Mascotas 		2) y el guion es de Mike White, ganador de dos Emmy y creador de la serie The White Lotus, y del veterano guionista de todas las anteriores entregas de Gru, mi villano 		favorito, Ken Daurio.",
+    		"Español, Ingles", "/images/peli2.jpg", "Mi villano Favorito 4", "https://www.youtube.com/watch?v=I0_qFoptZ4Y");
+
---
+++ Script Database/Query insert compra.sql
@@ -0,0 +1,35 @@
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(143, 20, "11/28", 1, 1, "jorge", "2323 3476 7878 8787");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(123,20,"01/25",1,2,"mariela martinez", "8929 3833 9393 9999");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(929, 20, "09/27", 6,2, "alberto campos", "4152 9292 9292 9292");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(234, 30,"12/29", 2, 1, "Omar Camacho", "2312 3303 0303 0303");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(134, 20, "10/27", 5,1,"Kevin Quispe", "4232 9393 9393 9393");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(143,20,"11/28", 1, 2, "Raul Aparicio", "2323 3476 7878 8787");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(123, 400, "09/30", 7, 2, "david rodriguez", "4152 9293 8392 0338");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(123, 300, "12/31", 8, 2, "pablo rojas", "3234 9348 8449 4994");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(123, 20, "12/28", 9, 2, "rocio becerra", "9192 8282 9282 2828");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(123, 20, "11/27", 5, 1, "axel gael", "9299 2929 2929 2922");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(292, 20, "12/28", 3, 1, "romeo lopez", "9384 8489 3933 9933");
+
+INSERT INTO Compra(cvv, total, fecha_vencimiento, id_movie, id_user, nombre, num_tarjeta)
+values(123, 300,  "03/29", 8, 1, "jorge aguilar", "9292 9292 9292 9292");
